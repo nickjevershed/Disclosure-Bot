af639ac61a8480cd95d059c4e1c3d9084a4b1bdb
Nick Evershed <nick.evershed@guardiannews.com>
2016-01-17 22:43:39 +1100

    working scraper, with json for clients and lobbyists

--- lobbyists.py
+++ lobbyists.py
@@ -8,6 +8,13 @@ import lxml.html
 from datetime import datetime
 import json
 
+def cleanText(text):
+    if text:
+        return text.strip()
+    else:
+        return text    
+
+
 def scrapeLobbyists():
     dateScraped = datetime.strftime(datetime.now(), '%Y-%m-%d')
     lobbyistUrl = "http://lobbyists.pmc.gov.au/who_register.cfm"
@@ -32,7 +39,11 @@ def scrapeLobbyists():
             agencyName = tds[2].cssselect("a")[0].text.strip()
             print agencyName
 
-            agencyAbn = tds[3].text.strip().replace(" ","")
+
+            agencyAbn = tds[3].text
+
+            if agencyAbn:
+                agencyAbn = cleanText(agencyAbn).replace(" ","")
 
             agencyUrl = "http://lobbyists.pmc.gov.au/" + tds[2].cssselect("a")[0].attrib['href']
             print agencyUrl
@@ -48,6 +59,23 @@ def scrapeLobbyists():
             for clientTr in clientTrs[1:]:
                 clients.append(clientTr.cssselect("td")[1].text)
 
+            lobbyistTrs = clientRoot.cssselect("#lobbyistDetails tr")    
+
+            lobbyists = []    
+
+            for lobbyistTr in lobbyistTrs[1:]:
+                
+                lobbyistInfo = {}
+                lobbyistTds = lobbyistTr.cssselect("td")
+                lobbyistName = lobbyistTds[1].text.strip()
+                position = lobbyistTds[2].text
+
+                lobbyistInfo['lobbyistName'] = lobbyistName
+                lobbyistInfo['position'] = cleanText(lobbyistTds[2].text)
+                lobbyistInfo['governmentRepresentative'] = lobbyistTds[3].text
+                lobbyistInfo['cessationDate'] = cleanText(lobbyistTds[4].text)
+                lobbyists.append(lobbyistInfo)
+
             print clients 
 
             data = {}
@@ -56,7 +84,8 @@ def scrapeLobbyists():
             data['agencyAbn'] = agencyAbn
             data['agencyID'] = agencyID
             data['agencyUrl'] = agencyUrl
-            data['clients'] = clients
+            data['clients'] = str(clients)
+            data['lobbyists'] = str(lobbyists)
 
             print data
 
@@ -65,62 +94,52 @@ def scrapeLobbyists():
             if firstRun == True:
                 scraperwiki.sqlite.save(unique_keys=["agencyID","agencyUrl"], table_name="lobbyistsTable", data=data)
 
-            #if it's not the first run then check for new lobbyists   
+            # #if it's not the first run then check for new lobbyists   
 
-            elif firstRun == False:
-                queryString = "* from lobbyistsTable where agencyID='" + agencyID + "'"
-                queryResult = scraperwiki.sqlite.select(queryString)
+            # elif firstRun == False:
+            #     queryString = "* from lobbyistsTable where agencyID='" + agencyID + "'"
+            #     queryResult = scraperwiki.sqlite.select(queryString)
 
-                #if it hasn't been scraped before, save the values to the base database table and the table of lobbyists updates
+            #     #if it hasn't been scraped before, save the values to the base database table and the table of lobbyists updates
 
-                if not queryResult:
-                    print "new lobbyist, saving"
-                    scraperwiki.sqlite.save(unique_keys=["agencyID","agencyUrl"], table_name="lobbyistsTable", data=data)
-                    scraperwiki.sqlite.save(unique_keys=["agencyID","agencyUrl"], table_name="lobbyistsUpdateTable", data=data)
+            #     if not queryResult:
+            #         print "new lobbyist, saving"
+            #         scraperwiki.sqlite.save(unique_keys=["agencyID","agencyUrl"], table_name="lobbyistsTable", data=data)
+            #         scraperwiki.sqlite.save(unique_keys=["agencyID","agencyUrl"], table_name="lobbyistsUpdateTable", data=data)
 
 
-                #if it has been saved before, check if it has been updated
+            #     #if it has been saved before, check if it has been updated
                 
-                else:
-                    if data['dateUpdated'] != queryResult[0]['dateUpdated']:
+            #     else:
+            #         if data['dateUpdated'] != queryResult[0]['dateUpdated']:
 
-                        #it has been updated, so save the new values in the main database table
+            #             #it has been updated, so save the new values in the main database table
 
-                        print data['agencyName'], " has updated the lobbyists register"
+            #             print data['agencyName'], " has updated the lobbyists register"
 
-                        #check if they added or removed a client
+            #             #check if they added or removed a client
 
-                        oldClients = json.loads(queryResult[0]['dateUpdated'])
+            #             oldClients = json.loads(queryResult[0]['dateUpdated'])
 
-                        if set(oldClients) != set(clients):
+            #             if set(oldClients) != set(clients):
 
-                            # check if new clients have been added
+            #                 # check if new clients have been added
 
-                            if len(clients) > len(oldClients):
+            #                 if len(clients) > len(oldClients):
 
-                                changedClients = list(set(clients) - set(oldClients))
-                                clientType = 'addition'
+            #                     changedClients = list(set(clients) - set(oldClients))
+            #                     clientType = 'addition'
 
-                            # check if old clients have been removed    
+            #                 # check if old clients have been removed    
 
-                            elif len(oldClients) > len(clients): 
+            #                 elif len(oldClients) > len(clients): 
 
-                                changedClients = list(set(clients) - set(oldClients))
-                                clientType = 'removal'
+            #                     changedClients = list(set(clients) - set(oldClients))
+            #                     clientType = 'removal'
 
-                            # else added a new client and dropped an old client
+            #                 # else added a new client and dropped an old client
                             
-                            else:
-                                
-
-
-
-                        scraperwiki.sqlite.save(unique_keys=["agencyID","agencyUrl"], table_name="lobbyistsTable", data=data)
-
-                        #and save the update details in the update table
-
-                        scraperwiki.sqlite.save(unique_keys=["politicianName","interestsUrl","dateUpdated"], table_name="interestsUpdateTable", data=data)
-
-    print "Lobbyists register complete"
-
+            #                 else:
+            #                     #do something
+            #                     print "blah"
 scrapeLobbyists()
